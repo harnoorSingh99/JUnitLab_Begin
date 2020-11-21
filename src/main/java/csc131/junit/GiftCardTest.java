3e9c2a36d5456adac0d1e57ce3b37215d5f622c9
harnoor <harnoorjatts@gmail.com>
2020-11-21 14:48:42 -0800

    Added more test to cover all brances

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -55,4 +55,58 @@ public class GiftCardTest
 	        assertEquals("deduct(10.00)", 
 	                     s, card.deduct(10.0));
 	    }
+	    
+	    @Test
+	    public void deduct_AmountDue()
+	    {
+	        double       balance;        
+	        GiftCard     card;
+	        int          issuingStore;
+	        String       s;
+	        
+	        
+	        issuingStore = 1337;
+	        balance      = 100.00;
+	        card = new GiftCard(issuingStore, balance);
+
+	        s = "Amount Due: " + String.format("%6.2f", 10.00);
+	        assertEquals("deduct 110.00 from 100.00", 
+	                     s, card.deduct(110.0));
+	    }
+
+	    @Test
+	    public void deduct_InvalidTransaction()
+	    {
+	        double       balance;        
+	        GiftCard     card;
+	        int          issuingStore;
+	        String       s;
+	        
+	        
+	        issuingStore = 1337;
+	        balance      = 100.00;
+	        card = new GiftCard(issuingStore, balance);
+
+	        s = "Invalid Transaction";
+	        assertEquals("deduct -10.00 from 100.00", 
+	                     s, card.deduct(-10.0));
+	    }
+	    
+	    @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectBalance() throws IllegalArgumentException
+	    {
+	        new GiftCard(1, -100.00);
+	    }
+	    
+	    @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectID_Low()
+	    {
+	        new GiftCard(-1, 100.00);
+	    }
+	    
+	    @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectID_High()
+	    {
+	        new GiftCard(100000, 100.00);
+	    }
 }	 
